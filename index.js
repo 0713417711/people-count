13e4f6781b771ebd6136bd0650968f1fb385c0dd
Hillary Kirui <kipkorirhillary6@gmail.com>
2022-10-27 11:29:04 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-document.getElementById("count-el").innerText = 5;
+//document.getElementById("count-el").innerText = 5;
+let saveEL = document.getElementById("save-el")
+let count = 0;
+
+document.getElementById("save-el")
+
+function increment() {
+
+    count += 1;
+    document.getElementById("count-el").innerText = count;
+}
+
+function save() {
+    let countSep = count + " - "
+    document.getElementById("count-el").innerText = count;
+    saveEL.innerText += countSep;
+}
+ 
+ 
+
