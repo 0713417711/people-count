9811b00193337363bfddadeeea69edd099242f0d
Elite Lash <49905877+0713417711@users.noreply.github.com>
2022-10-27 12:00:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-//document.getElementById("count-el").innerText = 5;
+
 let saveEL = document.getElementById("save-el")
+let countEl = document.getElementById(count-el)
 let count = 0;
 
-document.getElementById("save-el")
+
 
 function increment() {
 
     count += 1;
-    document.getElementById("count-el").innerText = count;
+    countEl.innerText = count;
 }
 
 function save() {
     let countSep = count + " - "
-    document.getElementById("count-el").innerText = count;
-    saveEL.innerText += countSep;
+    saveEL.textCotent += countSep;
 }
  
  
